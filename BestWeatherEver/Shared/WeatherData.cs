e946d0710496d2c74f9dc27a69faca201552a0aa
Jonas Andersson <jonas.andersson@hiq.se>
2013-10-20 12:07:07 +0200

    Better string outputs for weather and wind description

--- BestWeatherEver/Shared/WeatherData.cs
+++ BestWeatherEver/Shared/WeatherData.cs
@@ -10,6 +10,68 @@ namespace BestWeatherEver.Core
 
 		public String Temperature { get; set; }
 
+		public String WindDirectionString ()
+		{
+			String direction = WindDirection;
+
+			if (direction == "N")
+				return "Norr";
+
+			direction = direction.Replace ("N", "1").Replace ("S", "2").Replace ("W", "3").Replace ("E", "4");
+			direction = direction.Replace ("1", "nord").Replace ("2", "syd").Replace ("3", "väst").Replace ("4", "ost");
+
+			return Helper.UppercaseFirst (direction);
+		}
+
+		public String TypeString ()
+		{
+			String simpleType = Type.Replace ("d", "").Replace ("n", "").Replace ("m", "");
+			switch (simpleType)
+			{
+			case "1":
+				return "Klart";
+			case "2":
+				return "Halvklart";
+			case "3":
+				return "Växlande molnighet";
+			case "4":
+				return "Mulet";
+			case "5":
+				return "Regnskurar";
+			case "6":
+				return "Regnskurar och åska";
+			case "7":
+				return "Lätt snöblandat regn";
+			case "8":
+				return "Snöbyar";
+			case "9":
+				return "Regn";
+			case "10":
+				return "Mycket regn";
+			case "11":
+				return "Regn och åska";
+			case "12":
+				return "Snöblandat regn";
+			case "13":
+				return "Snö";
+			case "14":
+				return "Snö och åska";
+			case "15":
+				return "Dimma";
+			case "20":
+				return "Lätt molnighet och åska";
+			case "21":
+				return "Snö och åska";
+			case "22":
+				return "Regn och åska";
+			case "23":
+				return "Snöblandat regn och åska";
+			default:
+				return "";
+			}
+
+		}
+
 		public WeatherData ()
 		{
 
